864bd2f309b273e99a300b51a9986f96847c53fc
castleaaron <castleaaron@icloud.com>
2024-08-14 22:10:06 -0500

    release

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import emailjs from 'emailjs-com';
 import Stars from './Stars';
 
 const Home = () => {
   const [quote, setQuote] = useState('');
 
   const text = "Hello! I am Aaron";
+  const [formData, setFormData] = useState({
+    to_name: 'Aaron',
+    name: '',
+    email: '',
+    message: ''
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,6 +38,31 @@ const Home = () => {
 
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.send('service_vy3ksei', 'template_spxm8i5', {
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+      to_name: formData.to_name
+    }, '8IWxddpXuzVXYi6jP')
+      .then((result) => {
+        console.log(result.text);
+        alert('Message sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send message.');
+      });
+  };
+
   return (
     <div className="App-header">
       <Stars />
@@ -50,6 +82,15 @@ const Home = () => {
 
       <p className = "Quote-header">Quote of the day:</p>
       <p className = "Quote">{quote}</p>
+      <div className="Contact">
+        <h2 className = "Contact-Header">Contact Me</h2>
+        <form className = "Contact-Form" onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Your Name" value={formData.name} onChange={handleChange} required />
+          <input type="email" name="email" placeholder="Your Email" value={formData.email} onChange={handleChange} required />
+          <textarea name="message" placeholder="Your Message" value={formData.message} onChange={handleChange} required />
+          <button type="submit">Send</button>
+        </form>
+        </div>
     </div>
   );
 };
