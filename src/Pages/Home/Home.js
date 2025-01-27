c9ab3859e724f8a0e9751d88f686939b175b798e
castleaaron <castleaaron@icloud.com>
2025-01-27 15:04:02 -0600

    idk

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,39 +6,56 @@ import './Home.css';
 
 const Home = () => {
   const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
 
-  const text = "Hello! I am Aaron";
-  const [formData, setFormData] = useState({
-    to_name: 'Aaron',
-    name: '',
-    email: '',
-    message: ''
-  });
+  // const text = "Hello! I am Aaron";
+  // const [formData, setFormData] = useState({
+  //   to_name: 'Aaron',
+  //   name: '',
+  //   email: '',
+  //   message: ''
+  // });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({
+  //     ...formData,
+  //     [name]: value
+  //   });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    emailjs.send('service_vy3ksei', 'template_spxm8i5', {
-      name: formData.name,
-      email: formData.email,
-      message: formData.message,
-      to_name: formData.to_name
-    }, '8IWxddpXuzVXYi6jP')
-      .then((result) => {
-        console.log(result.text);
-        alert('Message sent successfully!');
-      }, (error) => {
-        console.log(error.text);
-        alert('Failed to send message.');
-      });
-  };
+
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   emailjs.send('service_vy3ksei', 'template_spxm8i5', {
+  //     name: formData.name,
+  //     email: formData.email,
+  //     message: formData.message,
+  //     to_name: formData.to_name
+  //   }, '8IWxddpXuzVXYi6jP')
+  //     .then((result) => {
+  //       console.log(result.text);
+  //       alert('Message sent successfully!');
+  //     }, (error) => {
+  //       console.log(error.text);
+  //       alert('Failed to send message.');
+  //     });
+  // };
+
+    useEffect(() => {
+      fetch('http://localhost:5000/api/quote')
+        .then((response) => response.json())
+        .then((data) => {
+          setQuote(data.quote);
+          setAuthor(data.author);
+        })
+        .catch((error) => {
+          console.error('Error fetching quote:', error);
+        });
+    }, []);
+
+      
 
   return (
     
@@ -50,7 +67,8 @@ const Home = () => {
       </p>
 
       <p className="Quote-header">Quote of the day:
-      <p className="Quote">This is a test - Said by someone smart</p></p>
+      <p className="Quote">{quote} - {author}</p></p>
+
     </div>
   );
 };
