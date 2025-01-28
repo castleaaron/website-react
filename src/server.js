9102e017cb8d3693f96840f8861c7d09606cdcc3
castleaaron <castleaaron@icloud.com>
2025-01-28 13:09:08 -0600

    ignore

--- src/server.js
+++ src/server.js
@@ -5,13 +5,14 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const MONGODB = process.env.MONGODB
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://castleaaron:y4l0t80NDqJaCwYd@quotedb.pnwwd.mongodb.net/QuoteDB?retryWrites=true&w=majority')
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
